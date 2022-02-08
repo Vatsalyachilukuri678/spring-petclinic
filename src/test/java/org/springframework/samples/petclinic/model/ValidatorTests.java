8c51d259b871c9972b521bea7882960c3387aad8
Vatsalyachilukuri678 <vatsalyachilukuri678@gmail.com>
2022-02-09 03:24:51 +0530

    break a test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,9 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		//person.setFirstName("");
+            person.setFirstName("notempty");
+
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
